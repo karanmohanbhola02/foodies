acaf251c285ac0a79e34a9bc15ad85c04527a370
Karan Mohan <karan.bhola@simsim.in>
2020-07-25 13:54:22 +0530

    added tab bar images

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import RecipesNavigator from './RecipesNavigator';
 import RestaurantsNavigator from './RestaurantsNavigator';
 import { screenNames } from '../constants';
+import { palette } from '../theme'
 
 const Tab = createBottomTabNavigator();
 
 const RootNavigator = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                lazy={true}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused }) => {
+                        let iconName;
+                        if (route.name === screenNames.RECIPES) {
+                            iconName = focused ? require('../assets/icons/recipesSelected.png') : require('../assets/icons/recipes.png')
+                        } else if (route.name === screenNames.RESTAURANTS) {
+                            iconName = focused ? require('../assets/icons/restaurantsSelected.png') : require('../assets/icons/restaurants.png')
+                        }
+
+                        return <Image source={iconName} style={{width: 18, height: 18 }} resizeMode="contain"  />;
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: palette.darkLimeGreen,
+                    inactiveTintColor: palette.gray,
+                    showLabel: false
+                }}>
                 <Tab.Screen name={screenNames.RECIPES} component={RecipesNavigator} />
                 <Tab.Screen name={screenNames.RESTAURANTS} component={RestaurantsNavigator} />
             </Tab.Navigator>
---
+++ src/theme/palette.js
@@ -0,0 +1,8 @@
+const palette = {
+    black: '#1d1d1d',
+    white: '#ffffff',
+    darkLimeGreen: '#73c700',
+    gray: '#999999'
+};
+
+export default palette;
